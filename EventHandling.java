f3052a45c9dfbeed1405ced5328b0835d86b4459
frankappolonia <fappolonia@gmail.com>
2021-12-06 10:56:13 -0500

    exception handling
    ++ and // error caught

--- EventHandling.java
+++ EventHandling.java
@@ -12,13 +12,6 @@ public class EventHandling {
     public static void addValueToCalc(TextField f, String s){
         f.appendText(s);
     }
-
-
-    /**public static void calculateTotal(String final){
-        CalculatorUI.result.appendText(final);
-        
-      
-    } */
        
     public static void eval2(TextField equation, TextField result){
         /** This method is an attempt at using the javascript built in
@@ -27,22 +20,8 @@ public class EventHandling {
         String s;
         String eqTxt = equation.getText();
      
-        ScriptEngineManager mgr = new ScriptEngineManager();
-        ScriptEngine engine = mgr.getEngineByName("JavaScript");
-
-        try{
-      
-        
-        s = engine.eval(eqTxt).toString();
-        System.out.print(s);
+        s = EvaluateString.evaluateExpression(eqTxt);
         result.appendText(s);
-       
-    }
-    catch (ScriptException ex) {
-        Exception e = new Exception(ex);
-        System.out.print(e);
-        result.appendText("Invalid");
-    }
 }
 
     public static void clearCalc(TextField equation, TextField result){
